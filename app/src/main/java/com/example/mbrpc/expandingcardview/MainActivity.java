98350e4c40cc1f557f3e4e33e15e992de575c0a2
MarkTJBrown <mbrown48@qub.ac.uk>
2016-03-15 13:44:57 +0000

    Adding expanding cardview activity

--- app/src/main/java/com/example/mbrpc/expandingcardview/MainActivity.java
+++ app/src/main/java/com/example/mbrpc/expandingcardview/MainActivity.java
@@ -1,52 +1,162 @@
 package com.example.mbrpc.expandingcardview;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.CardView;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private CardView cardViewExpanded;
+    private ImageView toggle;
+    private TextView passed, warn, fail;
 
     @Override
+    @SuppressLint("SetJavaScriptEnabled")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        toggle = (ImageView)findViewById(R.id.imageViewExpand);
+        cardViewExpanded = (CardView)findViewById(R.id.card_graph_expanded);
+        cardViewExpanded.setVisibility(View.GONE);
+        // Handler to expand/contract card view
+        toggle.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                if (cardViewExpanded.getVisibility() == View.GONE) {
+                    expand();
+                }
+                else {
+                    collapse();
+                }
             }
         });
+
+        // Add graph to webView
+        final String myUrl = "file:///android_asset/chart.html";
+        WebView webViewCondensed = (WebView)findViewById(R.id.webViewCondensed);
+        webViewCondensed.getSettings().setJavaScriptEnabled(true);
+        webViewCondensed.loadUrl(myUrl);
+
+        // Text view listeners
+        passed = (TextView)findViewById(R.id.passed);
+        warn = (TextView)findViewById(R.id.warning);
+        fail = (TextView)findViewById(R.id.fail);
+        DetailsListener listener = new DetailsListener(passed, warn, fail);
+        passed.setOnTouchListener(listener);
+        warn.setOnTouchListener(listener);
+        fail.setOnTouchListener(listener);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void expand() {
+        // Set visible
+        cardViewExpanded.setVisibility(View.VISIBLE);
+
+        final int widthSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        final int heightSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        cardViewExpanded.measure(widthSpec, heightSpec);
+
+        ValueAnimator animator = slideAnimator(0, cardViewExpanded.getMeasuredHeight());
+        animator.start();
+        toggle.setImageResource(R.drawable.less);
+        rotate(-180.0f);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private void collapse() {
+        int finalHeight = cardViewExpanded.getHeight();
+        ValueAnimator animator = slideAnimator(finalHeight, 0);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                toggle.setImageResource(R.drawable.more);
+                rotate(180.0f);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                //Height=0, but it set visibility to GONE
+                cardViewExpanded.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+
+        animator.start();
+    }
+
+    private ValueAnimator slideAnimator(int start, int end) {
+        ValueAnimator animator = ValueAnimator.ofInt(start, end);
+
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                // Update height
+                int value = (Integer) animation.getAnimatedValue();
+                ViewGroup.LayoutParams layoutParams = cardViewExpanded.getLayoutParams();
+                layoutParams.height = value;
+                cardViewExpanded.setLayoutParams(layoutParams);
+            }
+        });
+
+        return animator;
+    }
+
+    private void rotate(float angle) {
+        Animation animation = new RotateAnimation(0.0f, angle, Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        animation.setFillAfter(true);
+        animation.setDuration(250);
+        toggle.startAnimation(animation);
+    }
+
+    public class DetailsListener implements View.OnTouchListener {
+        private TextView _passed, _warning, _fail;
+
+        public DetailsListener(TextView _passed, TextView _warning, TextView _fail) {
+            _passed = passed;
+            _warning = warn;
+            _fail = fail;
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            // TODO switch screen to "drill down"
+            switch(v.getId()) {
+                case R.id.passed:
+                    Toast.makeText(MainActivity.this, "Passed Clicked",
+                            Toast.LENGTH_LONG).show();
+                    break;
+                case R.id.warning:
+                    Toast.makeText(MainActivity.this, "Warning Clicked",
+                            Toast.LENGTH_LONG).show();
+                    break;
+                case R.id.fail:
+                    Toast.makeText(MainActivity.this, "Fail Clicked",
+                            Toast.LENGTH_LONG).show();
+                    break;
+            }
+            return true;
+        }
     }
 }
